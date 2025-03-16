ad5bfbfb4b1cbdd5305593e58e07fbf82ac4072a
Jonathan Hite <jhite@bebuggin.com>
2025-03-16 09:35:45 -0400

    deleted unused file

--- lib/utils/permission_util.dart
+++
@@ -1,6 +0,0 @@
-import 'package:permission_handler/permission_handler.dart';
-
-Future<bool> requestLocationPermission() async {
-  final status = await Permission.location.request();
-  return status.isGranted;
-}
